47aea3dd573388d43c54054fd9821e8843953eb6
prasad-in <98727163+prasad-in@users.noreply.github.com>
2023-05-17 12:44:35 +0530

    Update encrypt_decrypt.py

--- encrypt_decrypt.py
+++ encrypt_decrypt.py
@@ -54,7 +54,7 @@ def decrypt_password(encrypted_password, key):
 
 #key = b'myencryptionkey123'
 key = generate_key()
-password = 'testQL123'
+password = 'testQL123testQL123'
 
 
 encrypted_password = encrypt_password(password, key)
